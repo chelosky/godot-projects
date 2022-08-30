d5ca6784e15ad97acfc1de1ff5e727a02ff5aa40
Chelosky <marcelolazo19972013@hotmail.com>
2022-08-30 00:08:54 -0400

    :sparkles: added animation between levels

--- 002-Level Switcher/Levels/Level.gd
+++ 002-Level Switcher/Levels/Level.gd
@@ -4,5 +4,19 @@ signal level_changed(level_name)
 
 export (String) var level_name = "level"
 
+func _ready():
+	$Button.disabled = true
+
+func play_loaded_sound():
+	$LevelLoadedSound.play()
+	$Button.disabled = false
+
+func cleanup():
+	if $ButtonClickedSound.playing:
+		yield($ButtonClickedSound, "finished")
+	queue_free()
+
 func _on_Button_pressed():
+	$ButtonClickedSound.play()
+	$Button.disabled = true	
 	emit_signal("level_changed", level_name)
--- 002-Level Switcher/Levels/LevelSwitcher.gd
+++ 002-Level Switcher/Levels/LevelSwitcher.gd
@@ -1,12 +1,15 @@
 extends Node
 
+var next_level = null
+
 onready var current_level = $GrassLevel
+onready var anim = $AnimationPlayer
 
 func _ready():
 	current_level.connect("level_changed", self, "handle_level_changed")
+	current_level.play_loaded_sound()
 
 func handle_level_changed(current_level_name: String):
-	var next_level
 	var next_level_name: String 
 	
 	match current_level_name:
@@ -18,7 +21,20 @@ func handle_level_changed(current_level_name: String):
 			return
 	
 	next_level = load("res://Levels/"+ next_level_name +"Level.tscn").instance()
+	next_level.layer = -1
 	add_child(next_level)
+	anim.play('fade_in')
 	next_level.connect("level_changed", self, "handle_level_changed")
-	current_level.queue_free()
-	current_level = next_level
+
+func _on_AnimationPlayer_animation_finished(anim_name: String):
+	match anim_name:
+		"fade_in":
+			current_level.cleanup()
+			current_level = next_level
+			current_level.layer	= 1
+			next_level = null
+			anim.play("fade_out")
+		"fade_out":
+			current_level.play_loaded_sound()
+		_:
+			return
